f3da251193ac84420227a41b2fec4f9e751a8544
Dan Mazo <danmazo32@localhost.localdomain>
2020-01-16 10:49:18 +0800

    Implemented form for adding Logs

--- dashboard.php
+++ dashboard.php
@@ -31,6 +31,27 @@ if ($acct_type=="2")
         <div class="box-header" data-original-title>
             <h2><i class="halflings-icon white file"></i><span class="break"></span> Log Book </h2>
         </div>
+        <div class="" style="margin-top: 20px; margin-left: 10px;">
+            <form method="POST" action="" class="form-inline">
+                <label for="full_name">Full Name:</label>
+                <input type="text" name="full_name" id="full_name">
+                &nbsp;
+                <label for="equipment">Equipment borrowed:</label>
+                <input type="text" name="equipment" id="equipment">
+                &nbsp;
+                <label for="date_borrow">Date borrowed:</label>
+                <input type="date" name="date_borrow" id="date_borrow" class="input-medium">
+                &nbsp;
+                <label for="date_return">Date returned:</label>
+                <input type="date" name="date_return" id="date_return" class="input-medium">
+                &nbsp;
+                <label for="remark">Remarks:</label>
+                <input type="text" name="remark" id="remark" class="input-small">
+                &nbsp;
+                <button type="submit" class="btn">Submit</button>
+            </form>
+        </div>
+        
 
         <div class="box-content">
             <table class="table table-striped table-bordered bootstrap-datatable datatable">
